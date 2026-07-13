478ebf15b3906ba9b19f29a380cf428b64291f8b
Alexandru Marius Rădulescu <alex11amr110506@gmail.com>
2026-07-14 00:01:23 +0300

    spring mvc

---
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/alex/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,48 @@
+package com.alex.springboot.thymeleafdemo.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/showForm")
+    public String showForm(){
+        return "helloworld-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm(){
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShout(HttpServletRequest request, Model model){
+
+        String theName=request.getParameter("studentName");
+
+        theName=theName.toUpperCase();
+
+        String result= "Yo! " +theName;
+
+        model.addAttribute("message",result);
+
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormV3(@RequestParam("studentName") String theName, Model model){
+
+        theName=theName.toUpperCase();
+
+        String result= "Hey nigger! " +theName;
+
+        model.addAttribute("message",result);
+
+        return "helloworld";
+    }
+}
